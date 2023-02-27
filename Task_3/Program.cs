2bdc903d7001c238e29e443ae3b2f88d56176d5e
Elizaveta Romaniuk <elizavetasiniuk@gmail.com>
2023-02-27 12:45:12 +0300

    Task 3 done

---
+++ Task_3/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+int negative = 0, positive = 0;
+int[] arrayResult = new int[8];
+int[] GetArray()
+{
+    int[] array = new int[8];
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(0, 100);
+    return array;
+}
+void PrintResult(int[] array)
+{
+    Console.WriteLine($"Array result is {String.Join(" ", array)}");
+}
+arrayResult = GetArray();
+PrintResult(arrayResult);
+
